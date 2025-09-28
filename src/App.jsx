8f29f3efa30517a18b5583a757332b41eed6645f
faysal-hossain11 <devfaysalbd@gmail.com>
2025-09-28 23:48:10 +0600

    some update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import { Suspense, useState } from 'react';
 import './App.css'
 import User from './components/User';
@@ -12,6 +11,7 @@ import IplPlayers from './components/IPL/IplPlayers';
 import Header from './components/Header';
 import SelectedPlayers from './components/IPL/SelectedPlayers';
 import { ToastContainer } from 'react-toastify';
+import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 
 const fetchIPLPlayers = async () => {
@@ -27,7 +27,7 @@ const iplPlayersPromise = fetchIPLPlayers();
 function App() {
   const [toggle, setToggle] = useState(true);
   const [pursesPlayers, setPursesPlayers] = useState([]);
-  
+
 
   const handleDeletePlayer = (playerId) => {
     const removedPlayer = pursesPlayers.filter((player) => player?.id !== playerId)
@@ -35,10 +35,90 @@ function App() {
   }
 
 
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
+
   return (
     <>
       <Header />
-      <div className='max-w-[1440px] mx-auto'>
+
+
+      <ResponsiveContainer width="100%" height="100%">
+      <BarChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="pv" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
+        <Bar dataKey="uv" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
+      </BarChart>
+    </ResponsiveContainer>
+
+
+
+
+
+
+
+
+
+
+
+      {/* <div className='max-w-[1440px] mx-auto'>
 
         <div className='flex items-center justify-between mb-8'>
           <div>
@@ -56,7 +136,6 @@ function App() {
 
         </div>
 
-        {/* IPL Players Section */}
         {toggle ? (
           <Suspense fallback={<h2 className='text-2xl text-center'>Loading IPL Players...</h2>}>
             <IplPlayers
@@ -69,7 +148,7 @@ function App() {
 
 
 
-      </div>
+      </div> */}
 
       <ToastContainer />
     </>
